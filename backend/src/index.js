0c31c7ac2f6fb1be2bbb5160e54d716ec6a4c2c8
Satyam Rai <satyamrai553@gamil.com>
2024-12-24 13:50:40 +0530

    add cors to backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,9 @@
 import express from "express"
 import studentRouter from "./routes/student.routes.js"
+import cors from "cors"
 const app = express()
+
+app.use(cors());
 
 
 app.use("/api/v1/student", studentRouter);
